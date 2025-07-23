41b20ae8500258d212cc39819a35d32c284ec1e7
LucasRomano <lucasivanromano@gmail.com>
2025-07-23 13:22:36 +1000

    commit exam

--- front/src/constants/navLinks.tsx
+++ front/src/constants/navLinks.tsx
@@ -9,20 +9,6 @@ const generateLinksCategories = (cates: ICategory[]) => {
   }));
 };
 export const NavLinks = [
-  // { href: routes.profile,
-  //   label: "Perfil" },
 
-  // { href: routes.cart,
-  //   label: "Carrito" },
-
-  // { href: routes.login,
-  //     label: "Login"},
-
-  // { href: routes.products,
-  //    label: "Productos"
-  //    },
-  //   { href: routes.landing,
-  //     label: "Landing"
-  //    },
   ...generateLinksCategories(categories).slice(0, 5),
 ];
--- front/src/services/types.ts
+++ front/src/services/types.ts
@@ -1,8 +1,8 @@
-export interface LoginServiceResponse {
-    message: string;
-    data?: LoginResponse;
-    errors?: any;
-}
+// export interface LoginServiceResponse {
+//     message: string;
+//     data?: LoginResponse;
+//     errors?: unknown;
+// }
 
 export interface LoginResponse {
     login: boolean;
@@ -10,9 +10,81 @@ export interface LoginResponse {
     token: string;
 }
 
+// export interface IUser {
+//     id: number;
+//     username: string;
+//     email: string;
+//     // Add any additional properties required by your application, for example:
+//     firstName?: string;
+//     lastName?: string;
+//     roles?: string[];
+// }
+
+export interface Product {
+    // Define the properties of Product as needed, for example:
+    id: number;
+    name: string;
+    price: number;
+}
+
 export interface Category {
     
     id: number;
     name: string;
     products: Product[];
+}
+export interface IProduct  {
+    "id": number,
+    "name": string,
+    "description": string,
+    "price": number,
+    "stock": number,
+    "image": string,
+    "categoryId": number
+  }
+  enum eRole {
+    USER = "user",
+    ADMIN = "admin"
+  }
+
+  export type Params<T> = Promise<T>;
+  export type SearchParams<T> = Promise<T>;
+
+ export interface IOrder {
+  id: number;
+  status: string;
+  date: string;
+  products: IProduct[];
+}
+
+  export interface IUser {
+    id: number;
+    name: string;
+    email: string;
+    address: string;
+    phone: string;
+    role: eRole;
+    orders: IOrder[];
+  }
+ export interface RegisterUserDto {
+    name: string;
+    email: string;
+    password: string;
+    address: string;
+    phone: string;
+  }
+  export interface CreateOrderDto {
+    userId: number;
+    products: number[];
+  }
+ export interface LoginUserDto {
+    email: string;
+    password: string;
+  }
+  export interface ICategory {
+  
+    id: number;
+    name: string;
+    products: IProduct[];
+    
 }
